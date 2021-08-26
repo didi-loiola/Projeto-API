acabd9a0c574e2fe5ff500ac5abbda9d093ac644
Diego Loiola <diego.alves_1996@hotmail.com>
2021-08-26 13:08:45 -0300

    Adicionando as queries com acesso ao banco de dados

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,46 +1,102 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        messagem: 'Usando GET na rota de produtos'
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'select * from produtos;',
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(200).send({ response: resultado })
+            }
+        )
     })
+
 })
 
 router.post('/', (req, res, next) => {
-    const produto = {
-        nome: req.body.nome,
-        preco: req.body.preco
-    }
-    res.status(201).send({
-        messagem: 'Usando POST na rota de produtos',
-        produtoCriado: produto
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'INSERT INTO produtos(nome, preco) values (?,?)', [req.body.nome, req.body.preco],
+            (error, resultado, field) => {
+                conn.release();
+
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    })
+                }
+                res.status(201).send({
+                    messagem: 'Produto inserido com sucesso',
+                    id_produto: resultado.insertId
+                })
+            }
+        )
     })
 })
 
 router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto
-    if (id === 'especial') {
-        res.status(200).send({
-            messagem: 'Você descobriu o id especial',
-            id: id
-        })
-    } else {
-        res.status(200).send({
-            messagem: 'Você passou um id'
-        })
-    }
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'select * from produtos where id_produtos=?;', [req.params.id_produto],
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(200).send({ response: resultado })
+            }
+        )
+    })
 })
 
 router.delete('/', (req, res, next) => {
-    res.status(200).send({
-        messagem: 'Usando DELETE na rota de produtos'
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            `delete from produtos where id_produtos= ?`, [req.body.id_produto],
+            (error, resultado, field) => {
+                conn.release();
+
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    })
+                }
+                res.status(201).send({
+                    messagem: 'Produto removido com sucesso',
+                })
+            }
+        )
     })
 })
 
 router.patch('/', (req, res, next) => {
-    res.status(200).send({
-        messagem: 'Usando PATCH na rota de produtos'
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            `update produtos
+                set nome=?,
+                    preco=?
+            where   id_produtos= ?`, [
+                req.body.nome,
+                req.body.preco,
+                req.body.id_produto
+            ],
+            (error, resultado, field) => {
+                conn.release();
+
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    })
+                }
+                res.status(202).send({
+                    messagem: 'Produto atualizado com sucesso',
+                })
+            }
+        )
     })
 })
 
