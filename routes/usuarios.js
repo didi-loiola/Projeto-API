6d5475c108d7dca564c7685ffeca5bbd990c6c62
Diego Loiola <diego.alves_1996@hotmail.com>
2021-09-03 12:02:07 -0300

    Adicionada rota login no caminho usuarios atraves de um token

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 router.post('/cadastro', (req, res, next) => {
     mysql.getConnection((error, conn) => {
@@ -31,7 +32,38 @@ router.post('/cadastro', (req, res, next) => {
                     })
                 }
             })
+    })
+})
 
+router.post('/login', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        const query = `select * from usuarios where email = ?`
+        conn.query(query, [req.body.email], (error, results, fields) => {
+            if (error) { return res.status(500).send({ error: error }) }
+            if (results.length < 1) {
+                return res.status(401).send({ mensagem: 'Falha na autencicação' })
+            }
+            bcrypt.compare(req.body.senha, results[0].senha,
+                (err, result) => {
+                    if (err) {
+                        return res.status(401).send({ mensagem: 'Falha na autencicação' })
+                    }
+                    if (result) {
+                        const token = jwt.sign({
+                            id_usuario: results[0].id_usuario,
+                            email: results[0].email
+                        }, process.env.JWT_KEY, {
+                            expiresIn: "1h"
+                        })
+                        return res.status(200).send({
+                            mensagem: 'Autenticação com sucesso',
+                            token: token
+                        })
+                    }
+                    return res.status(401).send({ mensagem: 'Falha na autencicação' })
+                })
+        })
     })
 })
 
