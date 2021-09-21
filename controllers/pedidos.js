6401d82685bdadc9097f21587d7ab83179a8e327
Diego Loiola <diego.alves_1996@hotmail.com>
2021-09-21 10:38:44 -0300

    Adicionando no controller de pedidos os metodos oAuth, cobrancaPix, getQrCode e na rota pedidos referencia estes metodos

--- controllers/pedidos.js
+++ controllers/pedidos.js
@@ -1,4 +1,8 @@
 const mysql = require('../mysql');
+const fs = require('fs');
+const https = require('https');
+const axios = require('axios');
+const randexp = require('randexp');
 
 exports.getPedidos = async(req, res, next) => {
     try {
@@ -113,3 +117,126 @@ exports.deletePedido = async(req, res, next) => {
         return res.status(500).send({ error: error });
     }
 };
+
+exports.oAuth = async(req, res, next) => {
+    try {
+        const cert = fs.readFileSync('nome-do-certificado-aqui');
+        const credencials = process.env.CLIENT + ':' + process.env.CLIENT_SECRET;
+        const auth = Buffer.from(credencials).toString('base64');
+        const agent = https.Agent({
+            pfx: cert,
+            passphrase: ''
+        })
+        const data = JSON.stringify({ "grant_type": "client_credentials" })
+        res.locals.agent = agent;
+        const config = {
+            method: "POST",
+            url: process.env.URL_API_PAY,
+            headers: {
+                Authorization: 'Basic ' + auth,
+                'Content-type': 'application/json'
+            },
+            httpsAgent: agent,
+            data: data
+        }
+
+        axios(config)
+            .then(response => {
+                res.locals.accessToken = response.data.access_token;
+                console.log('autenticacao', response.data);
+                next();
+            })
+            .catch((error) => {
+                console.log(error);
+                return res.status(500).send({ error: error })
+            })
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+}
+
+exports.cobrancaPix = async(req, res, next) => {
+    try {
+        const data = JSON.stringify({
+            "calendario": { "expiracao": 3600 },
+            "devedor": {
+                "cpf": req.body.pagador.cpf,
+                "nome": req.body.pagador.nome
+            },
+            "valor": {
+                "original": req.body.value
+            },
+            "chave": process.env.CHAVE_PIX,
+            "solicitacaoPagador": req.body.descricao
+        })
+
+        const txId = new randexp(/^[a-zA-Z0-9]{26,25}$/).gen();
+
+        const config = {
+            method: "PUT",
+            url: process.env.URL_API_PAY + `${txId}`,
+            headers: {
+                Authorization: 'Bearer ' + res.locals.accessToken,
+                'Content-type': 'application/json'
+            },
+            httpsAgent: res.locals.agent,
+            data: data
+        }
+
+        axios(config)
+            .then(response => {
+                res.locals.conta = response.data;
+                console.log('conta', response.data);
+                next();
+            })
+            .catch((error) => {
+                console.log(error);
+                return res.status(500).send({ error: error })
+            })
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+}
+
+exports.getQrCode = async(req, res, next) => {
+    try {
+        const locId = res.locals.conta.loc.id;
+        const config = {
+            method: "GET",
+            url: process.env.URL_API_PAY + `${locId}` + '/qrcode',
+            headers: {
+                Authorization: 'Bearer ' + res.locals.accessToken,
+                'Content-type': 'application/json'
+            },
+            httpsAgent: res.locals.agent,
+        }
+
+        axios(config)
+            .then(response => {
+                res.locals.conta = response.data;
+                imageQrCode = decodeBase64Image(response.date.imagemQrCode);
+                fs.writeFileSync(`qrcode/pix-conta-${locId}.jpg`, imagemQrCode.data)
+                return res.status(200).send(response);
+            })
+            .catch((error) => {
+                console.log(error);
+                return res.status(500).send({ error: error })
+            })
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+}
+
+function decodeBase64Image(dataString) {
+    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    var response = {};
+
+    if (matches.length !== 3) {
+        return new Error('Invalid input string');
+    }
+
+    response.type = matches[1];
+    response.data = new Buffer(matches[2], 'base64');
+
+    return response;
+}
--- routes/pedidos.js
+++ routes/pedidos.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const pedidos = require('../controllers/pedidos');
+const login = require('../middleware/login');
 
 router.get('/', pedidos.getPedidos);
 router.post('/', pedidos.postPedidos);
 router.get('/:id_pedido', pedidos.getPedidoUnico);
 router.delete('/', pedidos.deletePedido);
+
+router.post('/:id_pedido/pix/cobranca', pedidos.oAuth, pedidos.cobrancaPix, pedidos.getQrCode);
 
 module.exports = router;
